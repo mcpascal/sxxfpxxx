e34faff3a38fce99b276b9d518347322b90ff678
Yu <yuwenbin.dj@gmail.com>
2017-02-18 19:59:43 +0800

    db class

--- class/MysqlDb.php
+++ class/MysqlDb.php
@@ -8,11 +8,56 @@
  */
 class MysqlDb
 {
-    public $link;
-    public function __construct($db_host,$db_user,$db_pass,$db_name,$db_port=3306,$db_charset='utf8'){
-        $this->link = mysql_connect($db_host.':'.$db_port,$db_user,$db_pass) or die('连接数据库失败');
-        mysql_query('set names '.$db_charset);
-        mysql_select_db($db_name);
+    private $link;
+    static private $instance;
+
+    // 连接数据库
+    private function __construct($host, $username, $password,)
+    {
+        $this->link = mysql_connect($host, $username, $password);
+        $this->query("SET NAMES 'utf8'", $this->link);
+        //echo mysql_errno($this->link) . ": " . mysql_error($link). "n";
+        //var_dump($this->link);
+        return $this->link;
+    }
+    private function __clone()
+    {
+    }
+    public static function get_class_nmdb($host, $username, $password)
+    {
+        //$connector = new nmdb($host, $username, $password);
+        //return $connector;
+
+        if (FALSE == (self::$instance instanceof self)) {
+            self::$instance = new self($host, $username, $password);
+        }
+        return self::$instance;
+    }
+    // 连接数据表
+    public function select_db($database)
+    {
+        $this->result = mysql_select_db($database);
+        return $this->result;
+    }
+    // 执行SQL语句
+    public function query($query)
+    {
+        return $this->result = mysql_query($query, $this->link);
+    }
+    // 将结果集保存为数组
+    public function fetch_array($fetch_array)
+    {
+        return $this->result = mysql_fetch_array($fetch_array, MYSQL_ASSOC);
+    }
+    // 获得记录数目
+    public function num_rows($query)
+    {
+        return $this->result = mysql_num_rows($query);
+    }
+    // 关闭数据库连接
+    public function close()
+    {
+        return $this->result = mysql_close($this->link);
     }
 
 }
--- config/config.php
+++ config/config.php
@@ -3,11 +3,11 @@ define('DB_HOST','localhost');
 define('DB_NAME','sxxfpxxx');
 define('DB_USER','root');
 define('DB_PWD','root');
-//define('DB_CHARSET','utf8');
+define('DB_CHARSET','utf8');
 define('DB_PORT','3306');
 
 
 require_once '../class/MysqlDb.php';
-$db = new MysqlDb(DB_HOST,DB_USER,DB_PWD,DB_NAME,DB_PORT);
+$db = MysqlDb(DB_HOST,DB_USER,DB_PWD,DB_NAME,DB_PORT,DB_CHARSET);
 $link = $db->link;
 var_dump($link);
