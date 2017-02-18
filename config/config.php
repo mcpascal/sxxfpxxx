25f89c7df78767db15d49b187b84e89f69c06657
Yu <yuwenbin.dj@gmail.com>
2017-02-18 19:21:08 +0800

    修改

--- config/config.php
+++ config/config.php
@@ -1,13 +1,13 @@
 <?php
 define('DB_HOST','localhost');
 define('DB_NAME','sxxfpxxx');
-define('DB_USER','homestead');
-define('DB_PWD','secret');
+define('DB_USER','root');
+define('DB_PWD','root');
 //define('DB_CHARSET','utf8');
 define('DB_PORT','3306');
 
 
 require_once '../class/MysqlDb.php';
-$db = new MysqlDb();
+$db = new MysqlDb(DB_HOST,DB_USER,DB_PWD,DB_NAME,DB_PORT);
 $link = $db->link;
 var_dump($link);
